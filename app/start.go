fa0ab00d05d56f8e871febb587339dc3858c00cb
Neil Smith <github@nsmith.me.uk>
2017-12-10 17:47:07 +0000

    Added expected XML(!) response type

--- app/start.go
+++ app/start.go
@@ -7,6 +7,7 @@ import (
 	"github.com/kataras/iris/mvc"
 	"fmt"
 	"os"
+	"encoding/xml"
 )
 
 func Run() {
@@ -38,11 +39,18 @@ type RootController struct {
 	mvc.C
 }
 
+type Response struct {
+	XMLName 	xml.Name 	`xml:"PublishResponse"`
+	Xmlns		string 		`xml:"xmlns,attr"`
+	MessageId	string		`xml:"PublishResult>MessageId"`
+	RequestId	string		`xml:"ResponseMetadata>RequestId"`
+}
+
 func (c *RootController) Get() string {
 	return "This is my default action..."
 }
 
-func (c *RootController) Post() int {
+func (c *RootController) Post() (string, string) {
 
 	message := "SNS message received"
 
@@ -61,5 +69,18 @@ func (c *RootController) Post() int {
 
 	//---
 
-	return 200
+	// Return the expected XML(!) response.
+
+	resp := &Response{
+		Xmlns: "http://sns.amazonaws.com/doc/2010-03-31/",
+		MessageId: NewUUID(),
+		RequestId: NewUUID(),
+	}
+
+	output, err := xml.MarshalIndent(resp, "", "    ")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	return string(output), "application/xml"
 }
