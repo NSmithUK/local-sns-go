8c49a9267134dd22dd95496bcf445becc11f8d2a
Neil Smith <git@nsmith.me.uk>
2018-03-02 15:20:34 +0000

    Moved iris -> Gin

--- app/start.go
+++ app/start.go
@@ -1,14 +1,18 @@
 package app
 
 import (
-	"github.com/kataras/iris"
-	"github.com/kataras/iris/middleware/logger"
-	"github.com/kataras/iris/middleware/recover"
-	"github.com/kataras/iris/mvc"
-	"fmt"
 	"os"
+	"github.com/gin-gonic/gin"
 	"encoding/xml"
+	"fmt"
 )
+
+type Response struct {
+	XMLName 	xml.Name 	`xml:"PublishResponse"`
+	Xmlns		string 		`xml:"xmlns,attr"`
+	MessageId	string		`xml:"PublishResult>MessageId"`
+	RequestId	string		`xml:"ResponseMetadata>RequestId"`
+}
 
 func Run() {
 
@@ -20,67 +24,38 @@ func Run() {
 
 	//---
 
-	app := iris.New()
+	router := gin.Default()
 
-	app.Logger().SetLevel("debug")
+	router.POST("/", func(c *gin.Context) {
 
-	app.Use(recover.New())
-	app.Use(logger.New())
+		message := "SNS message received"
 
-	app.Controller("/", new(RootController))
+		c.Request.ParseForm()
 
-	app.Run(iris.Addr(":" + port), iris.WithoutVersionChecker, iris.WithConfiguration(iris.Configuration{ // default configuration:
-		DisableInterruptHandler: true,
-	}))
-}
-
-
-type RootController struct {
-	mvc.C
-}
-
-type Response struct {
-	XMLName 	xml.Name 	`xml:"PublishResponse"`
-	Xmlns		string 		`xml:"xmlns,attr"`
-	MessageId	string		`xml:"PublishResult>MessageId"`
-	RequestId	string		`xml:"ResponseMetadata>RequestId"`
-}
-
-func (c *RootController) Get() string {
-	return "This is my default action..."
-}
-
-func (c *RootController) Post() (string, string) {
-
-	message := "SNS message received"
-
-	for key, values := range c.Ctx.FormValues() {
-
-		message += fmt.Sprintf("\n-------------------------\nKey: %s", key)
-
-		for _, value := range values {
+		for key, value := range c.Request.PostForm {
+			message += fmt.Sprintf("\n-------------------------\nKey: %s", key)
 			message += fmt.Sprintf("\nValue: %s", value)
 		}
-	}
 
-	message += "\n--------------------------"
+		message += "\n--------------------------"
 
-	c.Ctx.Application().Logger().Info(message)
+		fmt.Println(message)
+
+		resp := &Response{
+			Xmlns: "http://sns.amazonaws.com/doc/2010-03-31/",
+			MessageId: NewUUID(),
+			RequestId: NewUUID(),
+		}
+
+		output, _ := xml.MarshalIndent(resp, "", "    ")
+
+		c.Data(200, "application/xml; charset=utf-8", output)
+	})
 
 	//---
 
-	// Return the expected XML(!) response.
+	router.Run()
 
-	resp := &Response{
-		Xmlns: "http://sns.amazonaws.com/doc/2010-03-31/",
-		MessageId: NewUUID(),
-		RequestId: NewUUID(),
-	}
-
-	output, err := xml.MarshalIndent(resp, "", "    ")
-	if err != nil {
-		fmt.Printf("error: %v\n", err)
-	}
-
-	return string(output), "application/xml"
 }
+
+
